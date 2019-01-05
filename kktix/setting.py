ffa4b3ac31ace9ca920c5bb9156f8a4ede226bd1
Xavier Lau <c.estlavie@icloud.com>
2019-01-05 15:12:23 +0800

    useless

--- kktix/setting.py
+++ kktix/setting.py
@@ -4,9 +4,9 @@ PASSWORD = 'test12345'
 # [connect] and [read]
 TIMEOUT = (1, 1)
 
-BOOKING_PAGE = 'https://kktix.com/events/a0b55680/registrations/new'
-EVENT_ID = 'a0b55680'
+BOOKING_PAGE = 'https://kktix.com/events/momoiroclove-fds6c/registrations/new'
+EVENT_ID = 'momoiroclove-fds6c'
 
 # [id], [quantity] and [currency]
-PAYLOAD = '{"tickets":[{"id":124759,"quantity":1,"invitationCodes":[],"member_code":"","use_qualification_id":null}],"currency":"TWD","recaptcha":{},"agreeTerm":true}'
-PAYLOAD_WITH_CAPTCHA = '{{"tickets":[{{"id":124759,"quantity":1,"invitationCodes":[],"member_code":"","use_qualification_id":null}}],"currency":"TWD","recaptcha":{{}},"custom_captcha":"{}","agreeTerm":true}}'
+PAYLOAD = '{"tickets":[{"id":145810,"quantity":1,"invitationCodes":[],"member_code":"","use_qualification_id":null}],"currency":"TWD","recaptcha":{},"agreeTerm":true}'
+PAYLOAD_WITH_CAPTCHA = '{{"tickets":[{{"id":145810,"quantity":1,"invitationCodes":[],"member_code":"","use_qualification_id":null}}],"currency":"TWD","recaptcha":{{}},"custom_captcha":"{}","agreeTerm":true}}'
