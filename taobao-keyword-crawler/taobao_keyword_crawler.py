8bebac1d298556e776adb62e8032da7eba7ffd02
Xavier Lau <c.estlavie@icloud.com>
2019-01-16 04:04:12 +0800

    useless

--- taobao-keyword-crawler/taobao_keyword_crawler.py
+++ taobao-keyword-crawler/taobao_keyword_crawler.py
@@ -1,17 +1,250 @@
 import requests
-from time import sleep
-from selenium import webdriver
-from os import path
+from json import loads
+from re import search
+from threading import Thread
 
-page = 'https://s.taobao.com/search?q={}&sort=sale-desc'
+PAGE = 'https://s.taobao.com/search?q={}&sort=sale-desc&s={}'
+PROXY_API = 'http://api3.xiguadaili.com/ip/?tid=559814210857062&num=1&delay=1&category=2&protocol=https'
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/603.2.5 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.5",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+    "Mozilla/5.0 (iPad; CPU OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.0 Mobile/14F89 Safari/602.1",
+    "Mozilla/5.0 (Windows NT 6.1; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0",
+    "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1; rv:52.0) Gecko/20100101 Firefox/52.0",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/58.0.3029.110 Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/603.2.5 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.5",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36 OPR/46.0.2597.32",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/59.0.3071.109 Chrome/59.0.3071.109 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 OPR/45.0.2552.898",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36 OPR/46.0.2597.39",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.8",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36",
+]
+PROXIES = []
+ITEMS_DETAIL = []
+COOKIES = ''
 
-s = requests.session()
 
-s.headers = {
-    'User-Agent': 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15',
-    'Cookie': 'isg=BA0NW-D9bdChqMnane_cFzcuHi-H6kG8rBGltk-WM6RORin4Fz9hjEOUtFIgnVl0; JSESSIONID=FDB4A4E17B54ADF167B0EA0546016356; mt=ci=0_1; uc1=cookie16=URm48syIJ1yk0MX2J7mAAEhTuw%3D%3D&cookie21=W5iHLLyFeYZ1WM9hVnmS&cookie15=W5iHLLyFOGW7aA%3D%3D&existShop=false&pas=0&cookie14=UoTYMbtSsuN2KA%3D%3D&tag=8&lng=zh_CN; lastalitrackid=login.taobao.com; _cc_=VT5L2FSpdA%3D%3D; _l_g_=Ug%3D%3D; _nk_=c_est_laviexavier; cookie1=WvSXAMmIcHV89bhJ52wQzQKcAG5tglJR2JNP7BUGPwg%3D; cookie17=UUtJZXMmPUavuQ%3D%3D; cookie2=18e1a89fa68a305df7567a35d1848318; csg=47d83d8f; dnk=c_est_laviexavier; existShop=MTU0NzQ3NTA1OA%3D%3D; lgc=c_est_laviexavier; sg=r50; skt=741c939e1f16cc25; t=1d60d70700ddc96c75f07f49c9e414ad; tg=0; tracknick=c_est_laviexavier; uc3=vt3=F8dByE%2BluQSu8DlJVaw%3D&id2=UUtJZXMmPUavuQ%3D%3D&nk2=AEX5p0zcb460L%2F7OOY2ms58%3D&lg2=UIHiLt3xD8xYTw%3D%3D; unb=2322140705; x5sec=7b227365617263686170703b32223a223032643430316161626364343639386465353266393136666530346335616466434c757538754546454f7969696f69486d5a3757756745614444497a4d6a49784e4441334d4455374d673d3d227d; l=aBClWJAYyF0iDzLKLMaiDXDCvxrxygBPugO3xMwzHiYGdP8ZuXKbBCWvP_wwN2Rttq7so0dj9cB2.; _uab_collina=154747116048853104969016; alitrackid=www.taobao.com; _tb_token_=5156377eeee5e; v=0; ali_ab=14.21.201.175.1545302324373.6; hng=CN%7Czh-CN%7CCNY%7C156; cna=pBdzFKD9mTwCAQ4Vyb+9V15k; thw=cn; enc=%2F18KiLG2VjlWIIslbuWqnKaDybUhIVjKSmWzeq5nkdmMrjmE2iuAT%2BflkwSzUqXm2bLnyRmiqK6C9%2FqWpEwOaw%3D%3D; miid=1052939520957951070'
-}
+def get_proxy():
+    global PROXIES
 
-keyword = '温度计'
-resp = s.get(page.format(keyword)).text
-print(resp)
+    if not PROXIES:
+        PROXIES = requests.get(PROXY_API).text.splitlines()
+    return PROXIES.pop()
+
+
+def get_user_agent():
+    from random import choice
+
+    return choice(USER_AGENTS)
+
+
+def get(url, cookie=None, proxy=None, referer=None):
+    headers = {'User-Agent': get_user_agent()}
+
+    if cookie is not None:
+        headers.update({'Cookie': cookie})
+    if proxy is not None:
+        proxy = {'https': get_proxy()}
+    if referer is not None:
+        headers.update({'Referer': referer})
+
+    try:
+        return requests.get(
+            url,
+            headers=headers,
+            proxies=proxy,
+            timeout=(5, 5)
+        )
+    except requests.exceptions.RequestException as e:
+        # print(e)
+        return get(url)
+
+
+def get_items(url):
+    global COOKIES
+
+    resp = get(url, cookie=COOKIES)
+    matched = search(r'g_page_config = (.+?);\n', resp.text)
+    if matched is None:
+        COOKIES = input('Please update cookies: ')
+        return get_items(url)
+    else:
+        return loads(matched.group(1))
+
+
+def parse_item(text):
+    matched = search(r'({"valItemInfo.+?)\n', text)
+    if matched is None:
+        print(text)
+        raise ValueError
+    else:
+        return loads(matched.group(1))['detail']
+
+
+def get_sib(text):
+    from json.decoder import JSONDecodeError
+
+    matched = search(r'//detailskip.taobao.com.+?itemId=(\d+)&sellerId=(\d+).+?tradeContract', text)
+    item_id = matched.group(1)
+    seller_id = matched.group(2)
+    resp = get(
+        f'https://detailskip.taobao.com/service/getData/1/p2/item/detail/sib.htm?itemId={item_id}&sellerId={seller_id}&modules=price',
+        referer=f'https://item.taobao.com/item.htm?id={item_id}',
+    )
+    try:
+        return loads(resp.text)
+    except JSONDecodeError:
+        print(resp.text)
+
+
+def parse_detail(auction):
+    title = auction['raw_title']
+    url = f'https:{auction["detail_url"]}'
+    price = auction['view_price']
+    sales = auction['view_sales'][:-3] if 'view_sales' in auction else ''
+    location = auction['item_loc']
+    nick = auction['nick']
+
+    item = get(url).text
+    if 'tmall' in url:
+        detail = parse_item(item)
+        item_price = detail['defaultItemPrice'].split(' - ')
+    elif 'taobao' in url:
+        sib = get_sib(item)
+        item_price = sib['data']['price'].split(' - ')
+    else:
+        print(url)
+        raise ValueError
+
+    lowest_price = item_price[0]
+    if len(item_price) == 1:
+        highest_price = item_price[0]
+    else:
+        highest_price = item_price[1]
+
+    print(url, title, price, sales, location, nick, lowest_price, highest_price)
+    return [keyword, url, title, price, sales, location, nick, lowest_price, highest_price]
+
+
+def parse_chunk(chunk):
+    for auction in chunk:
+        ITEMS_DETAIL.append(parse_detail(auction))
+
+
+def new_csv():
+    import csv
+    from datetime import datetime
+    from os import path
+
+    file_name = datetime.now().strftime("%Y_%m_%d")
+    is_new_file = not path.isfile(file_name)
+
+    if is_new_file:
+        with open(f'{file_name}.csv', 'a', encoding='utf-8-sig', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['关键词', '链接', '标题', '价格', '付款数', '发货地', '店铺名', '最低价', '最高价'])
+
+    return file_name
+
+
+def save_to_csv(file_name):
+    import csv
+
+    with open(f'{file_name}.csv', 'a', encoding='utf-8-sig', newline='') as f:
+        writer = csv.writer(f)
+        for item in ITEMS_DETAIL:
+            writer.writerow(item)
+
+
+if __name__ == '__main__':
+    csv = new_csv()
+
+    with open('keywords.txt', 'r') as f:
+        keywords = f.read().splitlines()
+
+    for keyword in keywords:
+        for page in range(2):
+            page = get_items(PAGE.format(keyword, page * 44))
+            auctions = page['mods']['itemlist']['data']['auctions']
+            auctions_chunks = [auctions[i:i + 1] for i in range(0, len(auctions), )]
+            threads = []
+
+            for auctions_chunk in auctions_chunks:
+                thread = Thread(target=parse_chunk, args=(auctions_chunk,))
+                thread.start()
+                threads.append(thread)
+
+            for thread in threads:
+                thread.join()
+
+            save_to_csv(csv)
+            ITEMS_DETAIL.clear()
+            if len(auctions) < 44:
+                break
