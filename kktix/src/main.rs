ea0a39b84d73bd8a1926cf45308534bbd7df0110
Xavier Lau <c.estlavie@icloud.com>
2019-01-31 22:31:31 +0800

    useless

--- kktix/src/main.rs
+++ kktix/src/main.rs
@@ -25,92 +25,26 @@ fn default_client() -> Client {
 struct Kktix(Client);
 
 impl Kktix {
-    fn sign_in(username: &str, password: &str) -> Kktix {
-        let client = default_client();
-        let (headers, csrf) = {
-            let cookie;
-            let csrf;
-            loop {
-                if let Ok(mut resp) = client.get("https://kktix.com/users/sign_in").send() {
-                    if let Ok(text) = resp.text() {
-                        cookie = resp.headers()[reqwest::header::SET_COOKIE].to_owned();
-                        csrf = {
-                            // --- external ---
-                            use select::{
-                                document::Document,
-                                predicate::Name,
-                            };
+    fn from_cookie(cookie: &str) -> Kktix {
+        let mut headers = HeaderMap::new();
+        headers.insert(COOKIE, cookie.parse().unwrap());
 
-                            let document = Document::from(text.as_str());
-                            document.find(Name("meta"))
-                                .skip(13)
-                                .next()
-                                .unwrap()
-                                .attr("content")
-                                .unwrap()
-                                .to_owned()
-                        };
-
-                        break;
-                    }
-                }
-            }
-
-            println!("{:?}, {}", cookie, csrf);
-
-            let mut headers = HeaderMap::new();
-            headers.insert(reqwest::header::COOKIE, cookie);
-            println!("{:?}", headers);
-
-            (headers, csrf)
-        };
-
-        loop {
-            if let Ok(mut resp) = client.post("https://kktix.com/users/sign_in")
-                .form(&[
-                    ("utf8", "✓"),
-                    ("authenticity_token", &csrf),
-                    ("user[login]", username),
-                    ("user[password]", password),
-                    ("user[remember_me]", "0"),
-                    ("commit", "Sign+In"),
-                ])
-//                .body(format!(
-//                    "utf8=✓&authenticity_token={}&user[login]={}&user[password]={}&user[remember_me]=0&commit=Sign+In",
-//                    csrf,
-//                    username,
-//                    password
-//                ))
-                .headers(headers.clone())
-                .send() {
-                println!("{}", resp.text().unwrap());
-                println!("{:?}", client.get("https://kktix.com/users/edit").headers(headers.clone()).send().unwrap().url());
-                return Kktix(default_client());
-            }
-        }
+        Kktix(default_client(headers))
     }
-
-//    fn from_cookie(cookie: &str) -> Kktix {
-//        let mut headers = HeaderMap::new();
-//        headers.insert(COOKIE, cookie.parse().unwrap());
-//
-//        Kktix(default_client(headers))
-//    }
 }
 
 fn main() {
     // --- std ---
-//    use std::io::{Write, stdin, stdout};
-//
-//    let cookie = {
-//        print!("Cookie: ");
-//        stdout().flush().unwrap();
-//        let mut s = String::new();
-//        stdin().read_line(&mut s).unwrap();
-//
-//        s.trim().to_owned()
-//    };
-//
-//    let user = Kktix::from_cookie(&cookie);
-    let user = Kktix::sign_in("test1234", "test1234");
+    use std::io::{Write, stdin, stdout};
+
+    let cookie = {
+        print!("Cookie: ");
+        stdout().flush().unwrap();
+        let mut s = String::new();
+        stdin().read_line(&mut s).unwrap();
+
+        s.trim().to_owned()
+    };
+
+    let user = Kktix::from_cookie(&cookie);
 }
